6f36a441bc783cd83dde79ca25ef98a5e4b8ae36
Oblutak1 <96445895+Oblutak1@users.noreply.github.com>
2022-05-01 16:14:46 +0200

    Add files via upload

---
+++ Menu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+func _on_Start_pressed():
+	get_tree().change_scene("res://World.tscn")
+
+func _on_Exit_pressed():
+	get_tree().quit()
+
+
+func _on_Restart_pressed():
+	get_tree().change_scene("res://World.tscn")
